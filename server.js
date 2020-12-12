b0f26fef7c5828403061da813b9764d85f98e2fa
yeseniasanchez <75147615+yeseniasanchez@users.noreply.github.com>
2020-12-12 20:14:20 -0300

    Desarrolllo backend

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require('express');
 const db = require('./models');
 const app = express()
 const bodyParser = require('body-parser');
+const controller = require('./controller/controller.js');
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -21,9 +22,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 '/api/auth/signin'
 */
 app.get('/', function(req, res) {
-    console.log("Estructura base del proyecto backend");
-    res.send("Estructura base del proyecto backend");
+    db.user.findAll().then(users => res.json(users))
 });
+app.post('/api/auth/signin', controller.signin);
 const port = 3000
 app.listen(port, () => {
     console.log(`Running on http://localhost:${port}`)
